2d177449928ee9ba1a84538653926a204437756c
杨祺 <yangqi@yangqideMacBook-Air.local>
2018-02-04 20:53:00 +0800

    server init

--- weapp-server/index.js
+++ weapp-server/index.js
@@ -1,9 +1,18 @@
 let express = require('express');
 let app = express();
-let mongoose = require('mongoose');
+// let mongoose = require('mongoose');
+let bodyParser = require('body-parser');
+let cors = require('cors');
 
-mongoose.connect('mongodb://localhost:27017/weapp-server');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+// mongoose.connect('mongodb://localhost:27017/weapp-server');
+
+app.get('/',(req, res)=>{
+    res.send("Hello World!");
+});
 
 app.listen(3000, function () {
     console.log("Your server is running on port 3000");
-})
+});
