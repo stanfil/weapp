639abb03b09573b9220aaae57dfe1a9081199ce2
stanfil <352070351@qq.com>
2018-02-06 01:07:52 +0800

    new server & database

--- weapp-server/index.js
+++ weapp-server/index.js
@@ -1,17 +1,48 @@
 let express = require('express');
 let app = express();
-// let mongoose = require('mongoose');
+let mongoose = require('mongoose');
 let bodyParser = require('body-parser');
 let cors = require('cors');
-
+let uri = require('./config.js').uri;
+let Course = require('./models/course.js');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
-// mongoose.connect('mongodb://localhost:27017/weapp-server');
+mongoose.connect(uri);
 
-app.get('/',(req, res)=>{
+let db = mongoose.connection;
+db.on('error', function (err) {
+    console.log('connection failed!', err);
+});
+db.once('open', function () {
+    console.log('success!');
+});
+
+let course = new Course({
+    id: "15",
+    cover: "http://o86bpj665.bkt.clouddn.com/posters/chrome-devtools.png",
+    title: "Chrome 开发者工具",
+    price: 88,
+    intro: "",
+    introvideolink: "",
+    resourselink: "",
+    content: ""
+});
+course.save();
+
+app.post('/course/index',(req, res)=>{
+    // let data = req.body;
+    // console.log(data);
+    Course.find().select('id cover title').exec(function (err, courses) {
+        if(err) return  err;
+        res.send(courses);
+    });
+});
+
+app.post('/course/details',(req, res)=>{
     res.send("Hello World!");
 });
+
 
 app.listen(3000, function () {
     console.log("Your server is running on port 3000");
---
+++ weapp-server/models/course.js
@@ -0,0 +1,15 @@
+let mongoose = require("mongoose");
+let Schema = mongoose.Schema;
+
+let CourseSchema = new Schema({
+    id: { type: String, unique: true, required: true },
+    cover: { type: String },
+    title: { type: String, required: true},
+    price: { type: Number },
+    intro: { type: String },
+    introvideolink: { type: String },
+    resourselink: {type: String},
+    content: {type: String}
+});
+
+module.exports = mongoose.model('Course', CourseSchema);
